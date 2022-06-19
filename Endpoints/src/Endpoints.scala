b648634b60d92699d730008a701d2cd1d78487f7
Leon A. Becker <leonalexbecker@yahoo.de>
2022-06-19 16:11:28 +0200

    Group into multiple files

--- Endpoints/src/Endpoints.scala
+++ Endpoints/src/Endpoints.scala
@@ -1,9 +1,16 @@
 package de.neunzehnhundert97
 package endpoints
 
+/** An endpoint agreement where the client sends A and receives B. */
 trait Endpoint[+A, -B]:
-  def path: String
+  /** The HTTP path this endpoint is provided / invoked at. */
+  def path: String = s"/api/${toString}"
   def method: Method
 
-enum Method:
-  case GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS
+object Endpoint {
+  inline def apply[A, B](inline pathArg: String, inline methodArg: Method = Method.POST): Endpoint[A, B] =
+    new {
+      override def path   = pathArg
+      override def method = methodArg
+    }
+}
---
+++ Endpoints/src/Method.scala
@@ -0,0 +1,6 @@
+package de.neunzehnhundert97
+package endpoints
+
+enum Method {
+  case GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS
+}
